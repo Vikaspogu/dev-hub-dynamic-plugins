45e9ae1602baa37788f324bbf8eb45a5aa7e9dfb
Vikas Pogu <vikaspoguadf@gmail.com>
2024-04-10 08:29:36 -0500

    new folder

---
+++ rhdh-http-request/src/index.ts
@@ -0,0 +1,1 @@
+export * from "@roadiehq/scaffolder-backend-module-http-request"
---
+++ rhdh-opendora/src/index.ts
@@ -0,0 +1,2 @@
+export * from "@devoteam-nl/open-dora-backstage-plugin"
+export {default as OpenDoraIcon} from '@mui/icons-material/Analytics';
